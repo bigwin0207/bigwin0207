a4773c2cc828e8e7aaea10253e891c2333b9313d
DESKTOP-F4BC13L\hi <bigwin0207@naver.com>
2024-03-26 10:24:12 +0900

    no message

---
+++ chap04-conrtol-flow-lecture-source/src/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section02.looping;
+
+import com.ohgiraffers.section01.conditional.A_if;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        A_for a = new A_for();
+        //a.testSimpleForStatement();
+        //a.testForExample();
+        //a.testForExample2();
+        //a.testForExample3();
+        //a.printSimpleGugudan();
+
+        A_nestedFor b = new A_nestedFor();
+        // b.printGugudanFromTwoToNice();
+        //b.printStarInputRowTime();
+        // b.printTriangleStars();
+
+        B_while bn = new B_while();
+        //bn.testSimpleWhileStatement();
+        bn.testWhileExample();
+
+        C_doWhile c = new C_doWhile();
+        //c.testSimpleDoWhileStatement();
+        //c.testDoWhileExample();
+    }
+//dasdasdasdassad
+}
